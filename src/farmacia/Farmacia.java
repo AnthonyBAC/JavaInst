a63daa719a40047b7691c3279a538cb5de7e135f
anthony <anthony@ArchPc>
2024-08-25 14:36:30 -0400

    Terminado todos los print

--- src/farmacia/Farmacia.java
+++ src/farmacia/Farmacia.java
@@ -15,7 +15,19 @@ public class Farmacia {
         // TODO code application logic here
         
         Medicamento m1 = new Medicamento (11, " Aspirina", "", 100, new Date());
-        System.out.println(m1.toString());
+                System.out.println(m1.toString());
+        
+        Cliente c1 = new Cliente ("569 8888 9999 ,", "Andre ,","Paracetamol. " );
+                System.out.println(c1.toString());
+        
+        Proveedor_Farmacia p1 = new Proveedor_Farmacia ("001 ," , "Simi ,","Paracetamol.");
+                System.out.println(p1.toString());
+                
+        Sistema_Gestion_Inventario s1 = new Sistema_Gestion_Inventario ("Paracetamol ,", "Andre ,", "Simi.");
+                System.out.println(s1.toString());
+        
+       
+       
     }
    
     
